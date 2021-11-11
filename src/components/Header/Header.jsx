54543dd48188305aed25d3e2afd307b39be208a9
adjoo <adjo@ro.ru>
2021-11-11 21:45:49 +0300

    load results of react lesson 11

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://media.flaticon.com/dist/min/img/logo/flaticon_negative.svg" alt=""/>
+    </header>
+}
+
+
+export default Header;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Profile = () => {
+    return             <div className='content'>
+        <div className='content_wallpaper'>
+            <img src="https://images.ru.prom.st/789689695_w640_h640_kirpich-ruchnoj-formovki.jpg" alt=""/>
+        </div>
+        <div className="content_header">
+            ava + description
+        </div>
+        <div><h2>My posts</h2>
+            <div>post #1</div>
+            <div>post #2</div>
+            <div>post #3</div>
+
+        </div>
+
+    </div>
+}
+
+
+export default Profile;
